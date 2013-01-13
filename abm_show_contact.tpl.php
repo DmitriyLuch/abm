211af92e0f4d1c54a115bc2920d74a29e83b6346
DmitriyLuch <rashepkin_ds@ukr.net>
2013-01-13 22:52:17 +0200

    Looks like it is working

---
+++ abm_show_contact.tpl.php
@@ -0,0 +1,10 @@
+<div id="left-part"><img src="<?php print $photo;?>"/></div>
+<div id="right-part">
+    <div><?php print $category?></div>
+    <div><?php print $first_name?></div>
+    <div><?php print $last_name?></div>
+    <div><?php print $phone?></div>
+    <div><?php print $email?></div>
+    <div><?php print $birthday?></div>
+    <div><?php print $notes?></div>
+</div>
